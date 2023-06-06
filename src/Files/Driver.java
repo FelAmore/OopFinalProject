f817b124da632109b028dda05152587b682a546f
FelAmore <114073117+FelAmore@users.noreply.github.com>
2023-06-06 10:06:23 +0700

    Update Driver.java

--- src/Files/Driver.java
+++ src/Files/Driver.java
@@ -1,5 +1,5 @@
 public class Driver {
     public static void main(String args[]) {
-        new login().setVisible(true);
+        new login().setVisible(true); // open the login page to start
     }
 }
